be617378ed90af78f0ba533807a6fa774ddb4f44
Chigorgor Asadu <ccasadu15@gmail.com>
2020-02-04 19:56:25 +0100

    create sing beautyspot activity

--- app/src/main/java/com/chigo/beautyspot/single_beauty_spot_details/SingleBeautyspot.kt
+++ app/src/main/java/com/chigo/beautyspot/single_beauty_spot_details/SingleBeautyspot.kt
@@ -2,11 +2,12 @@ package com.chigo.beautyspot.single_beauty_spot_details
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chigo.beautyspot.R
 
-class SingleActivity : AppCompatActivity() {
+class SingleBeautyspot : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_single)
+        setContentView(R.layout.activity_single_beautyspot)
     }
 }
